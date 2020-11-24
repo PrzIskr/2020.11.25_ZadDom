e37b882415895a28747bcca9572c304715319fc5
PrzIskr <przemyslaw.iskra@student.up.krakow.pl>
2020-11-24 19:05:14 +0100

    aa

--- zad5a_Przemysław_Iskra.py
+++ zad5a_Przemysław_Iskra.py
@@ -3,8 +3,7 @@
 #grupa: L6
 # zadanie 5
 
-name_list = ['Damian', 'Ola', 'Barbara', 'Robert',
-'Zygmunt', 'Ewa']
+name_list = ['Damian', 'Ola', 'Barbara', 'Robert', 'Zygmunt', 'Ewa']
 
 letter_list = [name[0] for name in name_list]
 
