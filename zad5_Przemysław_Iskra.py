652f856ea635efd4136a653911bae96ab6e49aed
PrzIskr <przemyslaw.iskra@student.up.krakow.pl>
2020-11-24 17:02:01 +0100

    zadanie 5

--- zad5_Przemysław_Iskra.py
+++ zad5_Przemysław_Iskra.py
@@ -2,3 +2,11 @@
 #nr almumu: 145863
 #grupa: L6
 # zadanie 3
+
+name_list = ['Damian', 'Ola', 'Barbara', 'Robert',
+'Zygmunt', 'Ewa']
+
+letter_list = [name[0] for name in name_list]
+
+for letter in letter_list:
+    print(letter)
