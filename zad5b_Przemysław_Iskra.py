26518efe7d914b706f6029dd0a8d2ba22b2aec26
PrzIskr <przemyslaw.iskra@student.up.krakow.pl>
2020-11-24 19:02:32 +0100

    poprawne

--- zad5b_Przemysław_Iskra.py
+++ zad5b_Przemysław_Iskra.py
@@ -4,9 +4,12 @@
 # zadanie 5b
 
 list_len = 5
+list2_len = 4
+#i = 0
+#j = 0
+
 from random import randint
 
-rand_list = [randint(1, 10) for element in range (list_len)]
+rand_list = [[randint(1, 10) for element in range (list2_len)] for list2 in range (list_len)]
 
-for randoms in rand_list:
-    print(randoms)
+print(rand_list)
